d73a3184f8cdd7bd198e64251dc6b1b289f61585
Matthew Boston <matthew@matthewboston.com>
2017-01-06 17:54:05 -0700

    add flush and shutdown

--- src/analytics_clj/core.clj
+++ src/analytics_clj/core.clj
@@ -1,7 +1,18 @@
 (ns analytics-clj.core
-  (:import (com.segment.analytics Analytics)))
+  (:import (com.segment.analytics Analytics)
+           (com.segment.analytics.internal AnalyticsClient)))
 
 (defn initialize
   "Start building an Analytics instance."
   [write-key]
   (.build (Analytics/builder write-key)))
+
+(defn flush
+  "Flush events in the message queue."
+  [^AnalyticsClient client]
+  (.flush client))
+
+(defn shutdown
+  "Stops this instance from processing further requests."
+  [^AnalyticsClient client]
+  (.shutdown client))
