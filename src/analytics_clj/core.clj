fee9aa18b1d71e6d0f7ac02a761e2f52a515fc58
Matthew Boston <matthew@matthewboston.com>
2017-01-09 16:21:16 -0700

    use the Analytics class instead of AnalyticsClient

--- src/analytics_clj/core.clj
+++ src/analytics_clj/core.clj
@@ -1,7 +1,6 @@
 (ns analytics-clj.core
   (:require [analytics-clj.utils :refer [map->immutable-map]])
   (:import (com.segment.analytics Analytics)
-           (com.segment.analytics.internal AnalyticsClient)
            (com.segment.analytics.messages IdentifyMessage)))
 
 (defn initialize
@@ -13,21 +12,21 @@
   "`identify` lets you tie a user to their actions and
   record traits about them. It includes a unique User ID
   and any optional traits you know about them."
-  ([^AnalyticsClient client ^String user-id]
+  ([^Analytics client ^String user-id]
    (identify client user-id {}))
-  ([^AnalyticsClient client ^String user-id traits]
    (let [traits (map->immutable-map traits)]
      (.enqueue client (doto (IdentifyMessage/builder)
                         (.userId user-id)
                         (.traits traits))))))
+  ([^Analytics client ^String user-id traits]
 
 (defn track
   "`track` lets you record the actions your users perform.
   Every action triggers what we call an “event”, which can
   also have associated properties."
-  ([^AnalyticsClient client ^String user-id ^String event]
+  ([^Analytics client ^String user-id ^String event]
    (track client user-id event {}))
-  ([^AnalyticsClient client ^String user-id ^String event properties]
+  ([^Analytics client ^String user-id ^String event properties]
    ;; TODO
    ))
 
@@ -35,9 +34,9 @@
   "The `screen` method lets you you record whenever a user
   sees a screen of your mobile app, along with optional
   extra information about the page being viewed."
-  ([^AnalyticsClient client ^String user-id ^String name ^String category]
+  ([^Analytics client ^String user-id ^String name ^String category]
    (screen client user-id name category {}))
-  ([^AnalyticsClient client ^String user-id ^String name ^String category properties]
+  ([^Analytics client ^String user-id ^String name ^String category properties]
    ;; TODO
    ))
 
@@ -46,9 +45,9 @@
   a group. A group could be a company, organization, account,
   project or team! It also lets you record custom traits
   about the group, like industry or number of employees."
-  ([^AnalyticsClient client ^String user-id ^String group-id]
+  ([^Analytics client ^String user-id ^String group-id]
    (group client user-id group-id {}))
-  ([^AnalyticsClient client ^String user-id ^String group-id traits]
+  ([^Analytics client ^String user-id ^String group-id traits]
    ;; TODO
    ))
 
@@ -56,16 +55,16 @@
   "`alias` is how you associate one identity with another.
   This is an advanced method, but it is required to manage
   user identities successfully in some of our integrations."
-  [^AnalyticsClient client ^String previous-id ^String user-id]
+  [^Analytics client ^String previous-id ^String user-id]
   ;; TODO
   )
 
 (defn flush
   "Flush events in the message queue."
-  [^AnalyticsClient client]
+  [^Analytics client]
   (.flush client))
 
 (defn shutdown
   "Stops this instance from processing further requests."
-  [^AnalyticsClient client]
+  [^Analytics client]
   (.shutdown client))
