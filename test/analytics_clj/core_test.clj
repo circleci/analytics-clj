629719fd26a7d9e0b26869b56bacdb9a97b602fb
Matthew Boston <matthew@matthewboston.com>
2017-01-12 17:03:34 -0700

    we must namespace qualify private fns

--- test/analytics_clj/core_test.clj
+++ test/analytics_clj/core_test.clj
@@ -1,9 +1,9 @@
 (ns analytics-clj.core-test
   (:require [clojure.test :refer :all]
-            [analytics-clj.core :refer :all])
+            [analytics-clj.core :as a])
   (:import (java.util UUID)))
 
-(defonce analytics (initialize "foobarbaz"))
+(defonce analytics (a/initialize "foobarbaz"))
 
 (defmacro testing-void
   "When you want to test a Java function that returns void.
@@ -21,36 +21,36 @@
 
 (deftest test-identify
   (testing-void "we're able to identify a user"
-                (identify analytics "1234"))
+                (a/identify analytics "1234"))
 
   (testing-void "identify a user with traits"
-                (identify analytics "1234" {"email" "foo@bar.com"}))
+                (a/identify analytics "1234" {"email" "foo@bar.com"}))
 
   (testing "identify a user with keyword traits"
-    (with-redefs [traits* (fn [mb traits]
+    (with-redefs [a/traits* (fn [mb traits]
                             (is (= "email" (-> traits keys first))))]
-      (identify analytics "1234" {:email "foo@bar.com"})))
+      (a/identify analytics "1234" {:email "foo@bar.com"})))
 
   (testing "identify a user with namespaced keyword traits"
-    (with-redefs [traits* (fn [mb traits]
+    (with-redefs [a/traits* (fn [mb traits]
                             (is (= "email/address" (-> traits keys first))))]
-      (identify analytics "1234" {:email/address "foo@bar.com"})))
+      (a/identify analytics "1234" {:email/address "foo@bar.com"})))
 
   (testing-void "identify an anonymous user"
-                (identify analytics nil {} {:anonymous-id (UUID/randomUUID)})))
+                (a/identify analytics nil {} {:anonymous-id (UUID/randomUUID)})))
 
 (deftest test-track
   (testing-void "track a simple event"
-                (track analytics "1234" "signup"))
+                (a/track analytics "1234" "signup"))
 
   (testing "track an event with custom properties"
-    (with-redefs [properties* (fn [mb properties]
+    (with-redefs [a/properties* (fn [mb properties]
                                 (is (= "company" (-> properties keys first))))]
-      (track analytics "1234" "signup" {"company" "Acme Inc."})
-      (track analytics "1234" "signup" {:company "Acme Inc."})))
+      (a/track analytics "1234" "signup" {"company" "Acme Inc."})
+      (a/track analytics "1234" "signup" {:company "Acme Inc."})))
 
   (testing "disable an integration"
-    (with-redefs [enable-integration* (fn [mb k v]
+    (with-redefs [a/enable-integration* (fn [mb k v]
                                         (is (= "Amplitude" k))
                                         (is (= false v)))]
-      (track analytics "1234" "signup" {"company" "Acme Inc."} {:integrations {"Amplitude" false}}))))
+      (a/track analytics "1234" "signup" {"company" "Acme Inc."} {:integrations {"Amplitude" false}}))))
