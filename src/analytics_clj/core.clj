89da70a8f41ab3ffcced702c6bf5308ffbd2e6a3
Matthew Boston <matthew@matthewboston.com>
2017-01-13 10:37:13 -0700

    whitespace

--- src/analytics_clj/core.clj
+++ src/analytics_clj/core.clj
@@ -13,21 +13,25 @@
 
 (defn initialize
   "Start building an Analytics instance."
+
   [write-key]
   (.build (Analytics/builder write-key)))
 
 (defn enqueue
   "Top-level `enqueue` function to allow for extensibility in the future."
+
   [^Analytics analytics message-builder]
   (.enqueue analytics message-builder))
 
 (defn flush
   "Flush events in the message queue."
+
   [^Analytics analytics]
   (.flush analytics))
 
 (defn shutdown
   "Stops this instance from processing further requests."
+
   [^Analytics analytics]
   (.shutdown analytics))
 
@@ -41,23 +45,38 @@
 
 (defn common-properties
   "The `MessageBuilder` interface has a set of properties common to all messages."
+
   [message-builder {:keys [anonymous-id context integration-options integrations timestamp user-id]}]
   (doto message-builder
-    (cond-> (not (nil? anonymous-id)) (anonymous-id* anonymous-id))
-    (cond-> (not (nil? context)) (context* (merge ctx (string-keys context))))
-    (cond-> (not (nil? integration-options)) (enable-integration-options integration-options))
-    (cond-> (not (nil? integrations)) (enable-integrations integrations))
-    (cond-> (not (nil? timestamp)) (timestamp* timestamp))
-    (cond-> (not (nil? user-id)) (user-id* user-id))))
+    (cond-> (not (nil? anonymous-id))
+      (anonymous-id* anonymous-id))
+
+    (cond-> (not (nil? context))
+      (context* (merge ctx (string-keys context))))
+
+    (cond-> (not (nil? integration-options))
+      (enable-integration-options integration-options))
+
+    (cond-> (not (nil? integrations))
+      (enable-integrations integrations))
+
+    (cond-> (not (nil? timestamp))
+      (timestamp* timestamp))
+
+    (cond-> (not (nil? user-id))
+      (user-id* user-id))))
 
 (defn identify
   "`identify` lets you tie a user to their actions and
   record traits about them. It includes a unique User ID
   and any optional traits you know about them."
+
   ([^Analytics analytics user-id]
    (identify analytics user-id nil nil))
+
   ([^Analytics analytics user-id traits]
    (identify analytics user-id traits nil))
+
   ([^Analytics analytics user-id traits options]
    (enqueue analytics (doto (IdentifyMessage/builder)
                         (common-properties (merge {:user-id user-id} options))
@@ -67,10 +86,13 @@
   "`track` lets you record the actions your users perform.
   Every action triggers what we call an “event”, which can
   also have associated properties."
+
   ([^Analytics analytics user-id event]
    (track analytics user-id event nil nil))
+
   ([^Analytics analytics user-id event properties]
    (track analytics user-id event properties nil))
+
   ([^Analytics analytics user-id event properties options]
    (enqueue analytics (doto (TrackMessage/builder event)
                         (common-properties (merge {:user-id user-id} options))
@@ -80,10 +102,13 @@
   "The `screen` method lets you you record whenever a user
   sees a screen of your mobile app, along with optional
   extra information about the page being viewed."
+
   ([^Analytics analytics user-id name]
    (screen analytics user-id name nil nil))
+
   ([^Analytics analytics user-id name properties]
    (screen analytics user-id name properties nil))
+
   ([^Analytics analytics user-id name properties options]
    (enqueue analytics (doto (ScreenMessage/builder name)
                         (common-properties (merge {:user-id user-id} options))
@@ -94,10 +119,13 @@
   a group. A group could be a company, organization, account,
   project or team! It also lets you record custom traits
   about the group, like industry or number of employees."
+
   ([^Analytics analytics user-id group-id]
    (group analytics user-id group-id nil nil))
+
   ([^Analytics analytics user-id group-id traits]
    (group analytics user-id group-id traits nil))
+
   ([^Analytics analytics user-id group-id traits options]
    (enqueue analytics (doto (GroupMessage/builder group-id)
                         (common-properties (merge {:user-id user-id} options))
@@ -107,8 +135,10 @@
   "`alias` is how you associate one identity with another.
   This is an advanced method, but it is required to manage
   user identities successfully in some of our integrations."
+
   ([^Analytics analytics previous-id user-id]
    (alias analytics previous-id user-id nil))
+
   ([^Analytics analytics previous-id user-id options]
    (enqueue analytics (doto (AliasMessage/builder previous-id)
                         (common-properties (merge {:user-id user-id} options))))))
