6a278f7539795969b475b6b37c1285b08f7935e5
Matthew Boston <matthew@matthewboston.com>
2018-04-05 21:47:18 -0600

    add bond 0.2.6

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.segment.analytics.java/analytics "2.0.0-RC7"]]
+  :profiles {:dev {:dependencies [[bond "0.2.6"]]}}
   :plugins [[lein-codox "0.10.5"]]
   :codox {:output-path "docs"
           :namespaces [circleci.analytics-clj.core]})
