ff921fc7baee6faf86d0149b4c5f9bbab44c36b0
Matthew Boston <matthew@matthewboston.com>
2019-09-24 12:50:30 -0600

    update lein-codox 0.10.7

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.segment.analytics.java/analytics "2.1.1"]]
   :profiles {:dev {:dependencies [[bond "0.2.6"]]}}
-  :plugins [[lein-codox "0.10.5"]]
+  :plugins [[lein-codox "0.10.7"]]
   :codox {:output-path "docs"
           :namespaces [circleci.analytics-clj.core]})
