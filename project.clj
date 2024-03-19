7162e34f95b915cad4745fed74b30e43911a07ba
Stig Brautaset <stig@circleci.com>
2024-03-19 23:24:45 +0000

    Remove `lein-ancient` plugin and periodic CI job
    This seems less valuable now that we have Renovate to bump
    dependencies for us.

--- project.clj
+++ project.clj
@@ -7,7 +7,6 @@
                  [com.squareup.okio/okio-jvm "3.4.0"]
                  [com.segment.analytics.java/analytics "3.5.0"]]
   :profiles {:dev {:dependencies [[bond "0.2.6"]]}}
-  :plugins [[lein-ancient "0.6.15"]
-            [lein-codox "0.10.8"]]
+  :plugins [[lein-codox "0.10.8"]]
   :codox {:output-path "docs"
           :namespaces [circleci.analytics-clj.core]})
